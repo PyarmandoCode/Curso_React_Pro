041f7db42047736216d280a860b38c2be1d0c0ab
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-09-29 10:35:45 -0500

    React-Pro Day5

--- react-dia5-pro/src/components/app.js
+++ react-dia5-pro/src/components/app.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ApiComponent = () => {
     const [autos, setAutos] = useState([]);
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
+    const [searchInput, setSearchInput] = useState('');
+    const [filterdResults, setfilterdResults] = useState([])
 
 
     useEffect(() => {
@@ -26,34 +29,72 @@ const ApiComponent = () => {
         cargarAutosApi();
     }, [])
 
+    const SearchItems = (searchValue) => {
+        setSearchInput(searchValue)
+        if (searchInput !== '') {
+            const filteredData = autos.filter((item) => {
+                return Object.values(item).join('').toLocaleLowerCase().includes(searchInput.toLocaleLowerCase())
+            })
+            setfilterdResults(filteredData)
+        }
+        else {
+            setfilterdResults(autos)
+        }
+
+
+    }
+
 
     if (error) {
         return <>{error.message}</>
     } else if (!isLoaded) {
         return <><h2>Estamos Preparando Todo</h2></>
     } else {
-        return (
+    return (
+        <div style={{ padding: 20 }}>
+            Busca tu auto
+            <div className='container'>
+                <input icon="search"
+                    placeholder='Buscar..'
+                    onChange={(e) => SearchItems(e.target.value)}
+                />
+            </div>
             <div className="container">
-                <div className="row">
-                    {autos.map((item) => (
-                        <div className="col-md-4" key={item.auto}>
-                            <Card style={{ width: '18erm' }}>
-                                <Card.Img variant="top" src={item.imagen} alt={item.nombre}></Card.Img>
-                                <Card.Body>
-                                    <Card.Title>Modelo:{item.nombre}</Card.Title>
-                                    <Card.Text>${new Intl.NumberFormat('es-mx').format(item.precio)}</Card.Text>
-                                    <Button variant="primary">Ver Detalle</Button>
+                <div className='row' style={{ padding: '2erm' }}>
+                    {searchInput.length > 1 ? (
+                        filterdResults.map((item) => {
+                            return (
+                                <Card style={{ width: '18erm' }}>
+                                    <Card.Img variant="top" src={item.imagen} alt={item.nombre}></Card.Img>
+                                    <Card.Body>
+                                        <Card.Title>Modelo:{item.nombre}</Card.Title>
+                                        <Card.Text>${new Intl.NumberFormat('es-mx').format(item.precio)}</Card.Text>
+                                        <Button variant="primary">Ver Detalle</Button>
+                                    </Card.Body>
+                                </Card>
+                            )
+                        })
+                    ) : (
+                        autos.map((item) => {
+                            return (
+                                <Card style={{ width: '18erm' }}>
+                                    <Card.Img variant="top" src={item.imagen} alt={item.nombre}></Card.Img>
+                                    <Card.Body>
+                                        <Card.Title>Modelo:{item.nombre}</Card.Title>
+                                        <Card.Text>${new Intl.NumberFormat('es-mx').format(item.precio)}</Card.Text>
+                                        <Button variant="primary">Ver Detalle</Button>
 
-                                </Card.Body>
-                            </Card>
-                        </div>
-                    ))}
-
+                                    </Card.Body>
+                                </Card>
+                            )
+                        })
+                    )}
                 </div>
 
             </div>
-        )
-    }
+        </div>
+    )
+  }
 }
 
 export default ApiComponent;
