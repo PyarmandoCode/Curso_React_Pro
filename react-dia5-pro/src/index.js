b1ff8d6bdfa80f62d23184522cd35d5b3344501d
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-09-29 11:46:44 -0500

    React-pro Dia5

--- react-dia5-pro/src/index.js
+++ react-dia5-pro/src/index.js
@@ -1,15 +1,19 @@
 import React from  "react";
 import  ReactDOM  from "react-dom/client";
-import ApiComponent from "./components/app";
+//import ApiComponent from "./components/app";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AppStore from "./components/statestore"
+import store from "./components/store";
+import { Provider } from "react-redux";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 
 root.render(
-    <React.StrictMode>
-        <ApiComponent/>
-
-    </React.StrictMode>
+    <Provider store={store}>
+        <AppStore/>
+         
+    </Provider>
+    
 )
